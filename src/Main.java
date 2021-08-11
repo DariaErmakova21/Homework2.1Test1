79ec4b5380a7de7b91f78b3e0755fdff6653d08d
Daria Slizkaya <Slizkaya.daria21@gmail.com>
2021-08-11 12:41:09 +0300

    Resolve

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(String[] args) {
+        int amountTicket = 15000 ; //рублей
+        int oneMile = 20 ; //рублей
+        int amountMile = amountTicket / oneMile ;
+        System.out.println(amountMile) ;
+
+
+    }
 }
+
